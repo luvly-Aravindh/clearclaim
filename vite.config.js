893781dd604452f78316a2598802f57eab1d2538
getnos <getnosgitupio-hash@gmail.com>
2026-06-16 18:18:08 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // http://oldshares.clearclaim.in/
 export default defineConfig({
-  base: "/clearclaim-lp/",
+  base: "/https://oldshares.clearclaim.in/",
   plugins: [react()],
 });
